27aa14d03bd377dc65e3ee2202b8fbe2fa6898ea
touhid-maker <mdtouhidjomadder@gmail.com>
2025-12-09 04:23:20 +0600

    Base URL Fix

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from 'socket.io-client'
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4080" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4080" :  import.meta.env.VITE_API_URL.replace("/api", "");
 
 
 export const useAuthStore = create((set) => ({
