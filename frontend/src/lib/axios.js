ab5f7ca3da9f70e40d639b89fd1252da8f66e195
touhid-maker <mdtouhidjomadder@gmail.com>
2025-12-09 04:13:36 +0600

    imported vite url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios  from 'axios'
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4080/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4080/api" : import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
