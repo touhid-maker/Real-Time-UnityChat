d9a1c5a14bca8f77a4ed22bfd9f3f2b21eb8f81c
Md. Touhid Islam <75615166+touhid-maker@users.noreply.github.com>
2025-12-09 13:17:55 +0600

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/Message.js"
 import User from "../models/user.js"
-
+import { io, getReceiverSocketId } from "../lib/socket.js"
 
 export const getAllContacts = async (req, res)=> {
     try {
@@ -79,7 +79,10 @@ export const sendMessage = async (req, res)=> {
         await newMessage.save();
 
         // todo: send message in real-time if user is online - socket.io
-
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage)
     }catch(error){
