41fbe99b63eeee7a5f113a83bf3c68e402451080
Md. Touhid Islam <75615166+touhid-maker@users.noreply.github.com>
2025-12-09 17:50:21 +0600

    Update PageLoader.jsx

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,11 +1,18 @@
 import { LoaderIcon } from "lucide-react"
+const UnityChatLogo = "/images/UnityChatLogo.png"
 import './PageLoader.css'
 
-function PageLoader(){
-    return(
-        <div className="loader-container d-flex align-items-center justify-content-center gap-1">
-            <LoaderIcon size={30} className="loader-spin" />
-            <span className="fs-5 text-dark">Loading...</span>
+function PageLoader() {
+    return (
+        <div className="loader-container d-flex flex-column justify-content-center align-items-center">
+            <div className=" d-flex align-items-center justify-content-center gap-3">
+                <img src={UnityChatLogo} alt="UnitiyChat Logo" style={{ height: 'auto', width: '60px' }} />
+                <span className="fs-2 fw-bold my-2">UnityChat</span>
+            </div>
+            <div className=" d-flex align-items-center justify-content-center gap-1 mt-3">
+                <LoaderIcon size={30} className="loader-spin" />
+                <span className="fs-5 text-dark">Loading...</span>
+            </div>
         </div>
     )
 }
