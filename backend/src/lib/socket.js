df929f3896c76addbf72fe7f2b0191c0b6b91da3
Md. Touhid Islam <75615166+touhid-maker@users.noreply.github.com>
2025-12-09 13:21:46 +0600

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -28,7 +28,7 @@ const userSocketMap = {}; // {userId:socketId}
 io.on("connection", (socket) => {
   console.log("A user connected", socket.user.fullName);
 
-  const userId = socket.userId;
+const userId = socket.user._id.toString();
   userSocketMap[userId] = socket.id;
 
   // io.emit() is used to send events to all connected clients
