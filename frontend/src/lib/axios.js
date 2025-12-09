76f83b16d67435fd50517e40f2bf2d1aaf6b6416
Md. Touhid Islam <75615166+touhid-maker@users.noreply.github.com>
2025-12-09 12:33:37 +0600

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios  from 'axios'
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4080/api" : import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4080/api" : "https://real-time-unity-chat.vercel.app/",
     withCredentials: true,
 })
