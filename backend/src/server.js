6039af16d9f0397c0dff93ab7c173ddf2167ea91
Md. Touhid Islam <75615166+touhid-maker@users.noreply.github.com>
2025-12-09 15:36:26 +0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,23 +17,11 @@ const __dirname = path.resolve();
 
 
 
-// make ready for deployment
-// if(ENV.NODE_ENV=== "production"){
-//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//     // React routing fallback
-//     app.get("*", (_, res)=>{
-//         res.sendFile(path.join(path.join(__dirname, "../frontend/dist/index.html")));
-//     })
-// }
-
-
-
 
 
 app.use(express.json({ limit : '10mb'})); // req.body
 app.use(express.urlencoded({ limit : '10mb', extended: true }));
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}));
+app.use(cors({ origin: "https://real-time-unity-chat.vercel.app", credentials: true}));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
