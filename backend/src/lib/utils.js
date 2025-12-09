4e6f1d8a8b6549adac0a2fe6dfbb4c7a821e2147
Md. Touhid Islam <75615166+touhid-maker@users.noreply.github.com>
2025-12-09 15:40:39 +0600

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -16,7 +16,7 @@ export const generateToken = (userId, res)=> {
   res.cookie("jwt", token, {
     maxAge: 7*24*60*60*100, 
     httpOnly : true, // prevent XSS attackts: cross-site scripting
-    sameSite: "none", // CSRF Attacks
+    sameSite: "strict", // CSRF Attacks
     secure: ENV.NODE_ENV=== "development" ? false : true,
 
   });
